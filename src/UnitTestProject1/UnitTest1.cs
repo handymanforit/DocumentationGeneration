f7238c422c36f0ad40e9d555827ce4f663a06fda
Kris Nan <handymanforit@gmail.com>
2022-04-27 14:08:00 -0400

    Add files via upload

---
+++ src/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Example ex = new Example();
+            (double quotient, double remainder) = ex.Divide(10, 2);
+            Assert.AreEqual(5.0, quotient);
+            Assert.AreEqual(0.0, remainder);
+
+            (quotient, remainder) = ex.Divide(10, 3);
+            Assert.AreEqual(3.0, quotient);
+            Assert.AreEqual(1.0, remainder);
+        }
+    }
+}
